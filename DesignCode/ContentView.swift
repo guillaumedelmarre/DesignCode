8774dbb0428ca5129e71deee6dd4ebb25949aeb4
Guillaume Delmarre <guillaume.delmarre@icloud.com>
2020-04-19 17:53:40 +0800

    Initial Commit

---
+++ DesignCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCode
+//
+//  Created by Delmarre, Guillaume on 2020/4/19.
+//  Copyright © 2020 Delmarre, Guillaume. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
