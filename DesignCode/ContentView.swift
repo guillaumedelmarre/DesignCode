223644e0e6473cdaa58639f323684b3972ae4837
Guillaume Delmarre <guillaume.delmarre@icloud.com>
2020-04-19 18:52:24 +0800

    Initial setup

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -10,7 +10,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            
+            VStack {
+                Text("Card Back")
+            }
+            .frame(width: 300.0, height: 220.0)
+            .background(Color.blue)
+            .cornerRadius(10)
+            .shadow(radius: 20)
+            .offset(x: 0, y: -20)
+            
+            VStack {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("UI Design")
+                            .font(.headline)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color("accent"))
+                            .padding(.top)
+                        Text(/*@START_MENU_TOKEN@*/"Certificate"/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(.white)
+                            .padding(.top, 5.0)
+                    }
+                    Spacer()
+                    Image("Logo")
+                        .resizable()
+                        .frame(width: 30.0, height: 30.0)
+                }
+                .padding(.horizontal)
+                Spacer()
+                Image("Background")
+            }
+            .frame(width: 340.0, height: 220.0)
+            .background(Color.black)
+            .cornerRadius(10)
+            .shadow(radius: 20
+            )
+        }
     }
 }
 
