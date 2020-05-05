f68f1dea6371467274d8c386ac99683e6e66e18e
Guillaume Delmarre <guillaume.delmarre@icloud.com>
2020-05-05 18:00:04 +0800

    Home menu
    Created the Home view, and first used SF symbols.

---
+++ DesignCode/Home.swift
@@ -0,0 +1,52 @@
+//
+//  Home.swift
+//  DesignCode
+//
+//  Created by Delmarre, Guillaume on 2020/5/5.
+//  Copyright © 2020 Delmarre, Guillaume. All rights reserved.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20.0) {
+            MenuRow(image: "person.crop.circle", text: "My Account")
+            MenuRow(image: "creditcard", text: "Billing")
+            MenuRow(image: "person.and.person", text: "Team")
+            MenuRow(image: "arrow.uturn.down", text: "Sign out")
+            Spacer()
+        }
+        .padding(.top, 30)
+        .padding(30)
+        .frame(minWidth: 0, maxWidth: .infinity)
+        .background(Color.white)
+        .cornerRadius(30)
+        .padding(.trailing, 60)
+        .shadow(radius: 20)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
+
+struct MenuRow: View {
+//use these variables so that the content (icon and title) is not hardcoded
+    var image = "creditcard"
+    var text = "My Account"
+    
+    var body: some View {
+        HStack {
+            Image(systemName: image)
+                .imageScale(.large)
+                .foregroundColor(Color("icons"))
+                .frame(width: 32, height: 32)
+            Text(text)
+                .font(.headline)
+            Spacer()
+        }
+    }
+}
